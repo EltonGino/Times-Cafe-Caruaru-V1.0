f5fbc58276691563dea4fb6aa9a51287f270b9a2
EltonGino <95431149+EltonGino@users.noreply.github.com>
2023-12-05 13:12:31 -0300

    Update wapp-menu.js

--- js/wapp-menu.js
+++ js/wapp-menu.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
         element.addEventListener('click', (event) => {
             event.preventDefault(); // Impede o comportamento padrão do <a> de abrir uma pagina em branco
             const cafeNome = element.closest('.single-menu').querySelector('h4').innerText.trim();
-            const linkWhatsApp = `https://wa.me/5581992821891?text=Eu%20gostaria%20de%20comprar%20esse%20café%20${cafeNome}`;
+            const linkWhatsApp = `https://wa.me/5581999120838?text=Eu%20gostaria%20de%20comprar%20esse%20café%20${cafeNome}`;
             window.open(linkWhatsApp, '_blank');
         });
     });
